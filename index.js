11cbedcdcbec8e8b0dd81453bf61a6a949e2bca8
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-05-29 13:25:17 +0530

    use fs module

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-console.log("First Node program");
-const os = require("os");
-console.log(os.arch());
-console.log(os.hostname());
-console.log(os.type());
+// console.log("First Node program");
+// const { read } = require("fs");
+// const os = require("os");
+// console.log(os.arch());
+// console.log(os.hostname());
+// console.log(os.type());
+// repl=>read,eval,print,loop8
+
+const fs=require("fs");
+// fs.writeFileSync("read.txt","This is the first file created. ");
+// fs.appendFileSync("read.txt","data appended");
+const data=fs.readFileSync("read.txt");
+
+const original_data=data.toString();
+console.log(original_data);
+fs.renameSync("read.txt","readwrite.txt");
