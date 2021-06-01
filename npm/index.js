82d64575bed226e30c6b43d22aeeed5a47ad5aa2
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-06-01 22:09:42 +0530

    appy nodemon to index.js

--- npm/index.js
+++ npm/index.js
@@ -3,5 +3,5 @@ const validator=require("validator");
 
 console.log(chalk.red("Chalk module is used"))
 console.log(chalk.red.underline("Chalk module is used"))
-const res=validator.isEmail("m@g.com");
+const res=validator.isEmail("m@gmail.com");
 console.log(res);
