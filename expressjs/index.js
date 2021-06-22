ef17d1a036c75a0a60e6e06a2905056b553ff227
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-06-22 23:42:41 +0530

    start learning expressjs routing

--- expressjs/index.js
+++ expressjs/index.js
@@ -7,7 +7,15 @@ app.get("/",(req,res)=>{
 app.get("/about",(req,res)=>{
     res.send("Hello world from about page");
 });
-
+app.get("/html",(req,res)=>{
+    res.send("<h1>Hello world from html page</h1>");
+});
+app.get("/json",(req,res)=>{
+    res.send({
+        id:1,
+        name:"Mayank",
+    });
+});
 app.listen(8000,()=>{
     console.log("Listening the port at 8000");
 });
