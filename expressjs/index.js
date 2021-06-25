15f183b91449330c577876c1255f7d07c846da1d
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-06-26 01:34:06 +0530

    First hbs program

--- expressjs/index.js
+++ expressjs/index.js
@@ -4,9 +4,14 @@ const app=express();
 console.log(__dirname);
 const staticPath=path.join(__dirname,"public/");
 console.log(staticPath);
-app.use(express.static(staticPath));
+// app.use(express.static(staticPath));
 
-
+app.set("view engine",'hbs');
+app.get("/",(req,res)=>{
+    res.render("index.hbs",{
+        name:"Mayank",
+    });
+});
 app.get("/",(req,res)=>{
     res.send("Hello world from express");
 });
